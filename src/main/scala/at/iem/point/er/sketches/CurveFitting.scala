f12f563f59b352d74087142a7bcc6019d0df1e7d
Sciss <contact@sciss.de>
2013-02-26 17:57:48 +0100

    add linear fit

--- src/main/scala/at/iem/point/er/sketches/CurveFitting.scala
+++ src/main/scala/at/iem/point/er/sketches/CurveFitting.scala
@@ -1,18 +1,50 @@
 package at.iem.point.er.sketches
 
+import annotation.switch
+
 object CurveFitting {
   private type Points = Traversable[Point]
 
   final case class Point(x: Double, y: Double)
 
+  sealed trait Fit {
+    def order: Int
+    def apply(x: Double): Double
+  }
+
+  final case class PointFit(a: Double) extends Fit {
+    def order = 0
+    def apply(x: Double) = a
+
+    override def toString = f"f(x) = $a%1.3f"
+  }
+
+  /** Result of a linear polynomial fitting procedure.
+    *
+    * cf. http://www.efunda.com/math/leastsquares/lstsqr1dcurve.cfm
+    *
+    * @param a   the linear coefficient in `ax + b`
+    * @param b   the constant term in `ax + b`
+    */
+  final case class LinearFit(a: Double, b: Double) extends Fit {
+    def order = 1
+    def apply(x: Double): Double = a * x + b
+
+    override def toString = f"f(x) = $a%1.3fx + $b%1.3f"
+  }
+
   /** Result of a quadratic polynomial fitting procedure.
-   *
-   * @param a   the quadratic coefficient in `ax^2 + bx + c`
-   * @param b   the linear coefficient in `ax^2 + bx + c`
-   * @param c   the constant term in `ax^2 + bx + c`
-   * @param err the r-squared error which is 1 - (residual sum of squares / total sum of squares)
-   */
-  final case class QuadraticFit(a: Double, b: Double, c: Double, err: Double) {
+    *
+    * cf. http://www.efunda.com/math/leastsquares/lstsqr2dcurve.cfm
+    *
+    * @param a   the quadratic coefficient in `ax^2 + bx + c`
+    * @param b   the linear coefficient in `ax^2 + bx + c`
+    * @param c   the constant term in `ax^2 + bx + c`
+    * @param err the r-squared error which is 1 - (residual sum of squares / total sum of squares)
+    */
+  final case class QuadraticFit(a: Double, b: Double, c: Double, err: Double) extends Fit {
+    def order = 2
+
     override def toString = f"f(x) = $a%1.3fx^2 + $b%1.3fx + $c%1.3f (r^2 = $err%1.3f)"
 
     /** Calculates the dependent variable for a given domain point
@@ -20,10 +52,72 @@ object CurveFitting {
       * @param x  the position at which to evaluate the polynomial
       * @return   the fitted point for the given position
       */
-    def apply(x: Double): Double = a * (x*x) + b * x + c
+    def apply(x: Double) = a * (x*x) + b * x + c
   }
 
-  /** Fits a given set of points
+  def solve(points: Points, order: Int): Fit = (order: @switch) match {
+    case 0 => solvePoint(points)
+    case 1 => solveLinear(points)
+    case 2 => solveQuadratic(points)
+    case _ => throw new IllegalArgumentException(order.toString)
+  }
+
+  /** Fits a given set of points on a zeroth polynomial (point)
+   *
+   * @param points  the set of measured points
+   * @return        point fit
+   */
+  def solvePoint(points: Points): PointFit = {
+    require(points.size >= 1)
+
+    var s00   = 0           // sum of x^0 * y^0 = number of points
+    var s01   = 0.0         // sum of x^0 * y^1
+
+    points.foreach { p =>
+      s01 += p.y
+      s00 += 1
+    }
+
+    val a = s01 / s00       // plain mean, right?
+
+    PointFit(a)
+  }
+
+  /** Fits a given set of points on a first order polynomial (straight line)
+    *
+    * @param points  the set of measured points
+    * @return        coefficients of a line fit onto the set of points
+    */
+  def solveLinear(points: Points): LinearFit = {
+    require(points.size >= 2)
+
+    var s20   = 0.0         // sum of x^2 * y^0
+    var s10   = 0.0         // sum of x^1 * y^0
+    var s00   = 0           // sum of x^0 * y^0 = number of points
+
+    var s11   = 0.0         // sum of x^1 * y^1
+    var s01   = 0.0         // sum of x^0 * y^1
+
+    points.foreach { p =>
+      val x   = p.x
+      val y   = p.y
+      s10    += x
+      val x2  = x * x
+      s20    += x2
+
+      s11    += x * y
+      s01    += y
+
+      s00    += 1
+    }
+
+    val a = (s00 * s11 - s10 * s01) / (s00 * s20 - s10 * s10)
+    val b = (s01 * s20 - s10 * s11) / (s20 * s00 - s10 * s10)
+
+    new LinearFit(a = a, b = b)
+  }
+
+  /** Fits a given set of points on a second order polynomial (quadratic curve)
    *
    * @param points  the set of measured points
    * @return        coefficients of a quadratic polynomial fit onto the set of points
--- src/test/scala/at/iem/point/er/sketches/CurveFittingTest.scala
+++ src/test/scala/at/iem/point/er/sketches/CurveFittingTest.scala
@@ -8,28 +8,45 @@ import org.jfree.chart.renderer.xy.StandardXYItemRenderer
 
 object CurveFittingTest extends App {
   val n     = 20
-  val ys    = Vector.fill(n)(math.random * 100)
+  val ys    = List.fill(n)(math.random * 100 - 50).tails.map(_.sum).toList
   val pt    = ys.zipWithIndex.map(_.swap)
-  val fit   = CurveFitting.solveQuadratic(pt.map { case (x, y) => CurveFitting.Point(x,y) })
-  val fitp  = Vector.tabulate(pt.size * 10) { i => val x = i * 0.1; x -> fit(x) }
+  val ptc   = pt.map { case (x, y) => CurveFitting.Point(x,y) }
+  val qfit  = CurveFitting.solveQuadratic(ptc)
+  val qpt   = Vector.tabulate(pt.size * 10) { i => val x = i * 0.1; x -> qfit(x) }
+  val lfit  = CurveFitting.solveLinear(ptc)
+  val lpt   = Vector.tabulate(pt.size * 10) { i => val x = i * 0.1; x -> lfit(x) }
+  val pfit  = CurveFitting.solvePoint(ptc)
+  val ppt   = Vector.tabulate(pt.size * 10) { i => val x = i * 0.1; x -> pfit(x) }
 
-  println(fit)
+  println(qfit)
+  println(lfit)
+  println(pfit)
 
   Swing.onEDT {
     val data  = pt.toXYSeriesCollection("measured")
-    val dfit  = fitp.toXYSeriesCollection("fitted")
+    val dq    = qpt.toXYSeriesCollection("quadratic fit")
+    val dl    = lpt.toXYSeriesCollection("linear fit")
+    val dp    = ppt.toXYSeriesCollection("point fit")
     val chart = XYLineChart(null)
     val plot  = chart.plot
     plot.setDataset(0, data)
-    plot.setDataset(1, dfit)
+    plot.setDataset(1, dq)
+    plot.setDataset(2, dl)
+    plot.setDataset(3, dp)
 //    plot.getRenderer.setSeriesPaint(0, Color.blue)
 //    plot.getRenderer.setSeriesPaint(1, Color.red)
+    val r0 = new StandardXYItemRenderer()
+    r0.setSeriesPaint(0, Color.black)
+    plot.setRenderer(0, r0)
     val r1 = new StandardXYItemRenderer()
-    r1.setSeriesPaint(0, Color.blue)
-    plot.setRenderer(0, r1)
+    r1.setSeriesPaint(0, Color.red)
+    plot.setRenderer(1, r1)
     val r2 = new StandardXYItemRenderer()
-    r2.setSeriesPaint(0, Color.red)
-    plot.setRenderer(1, r2)
+    r2.setSeriesPaint(0, new Color(0x00, 0xC0, 0x00))
+    plot.setRenderer(2, r2)
+    val r3 = new StandardXYItemRenderer()
+    r3.setSeriesPaint(0, new Color(0x20, 0x40, 0xFF))
+    plot.setRenderer(3, r3)
 
     val f = chart.toFrame("Curve Fitting")
     f.pack()
