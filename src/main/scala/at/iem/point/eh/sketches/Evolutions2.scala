e1ac4d2ccf071cfb87619febf557debfc97ab037
Sciss <contact@sciss.de>
2013-03-27 17:12:23 +0100

    horiz dance factored out

--- src/main/scala/at/iem/point/eh/sketches/Evolutions2.scala
+++ src/main/scala/at/iem/point/eh/sketches/Evolutions2.scala
@@ -17,50 +17,14 @@ object Evolutions2 extends App {
   val sq        = loadSnippet(improvSnippets.last)
   val notesIn0  = sq.notes
   val (m, _)    = NoteUtil.splitMelodicHarmonic(notesIn0)
-  val notesIn   = m.flatMap(_._2)
-  val pitchSq   = notesIn.map(_.pitch.midi)
+  val mm        = m.map(_._2)
 
-  implicit val rnd = new util.Random(SEED)
-  val recPch    = ContextDance.move(pitchSq, num = NUM)(pitchSq(START) :: Nil)
-
-  val notesOut0 = recPch.zipWithIndex.map { case (midi, idx) =>
-    val off = idx * 0.25
-    OffsetNote(off, midi.asPitch, 0.125, 80)
-  }
-
-  val notesOut1 = if (VELO) {
-    val veloSq  = notesIn.map { n => val v = n.velocity; v - (v % VELO_COARSE) }
-    val recVelo = ContextDance.move(veloSq, num = NUM)(veloSq(START) :: Nil)
-    (notesOut0 zip recVelo).map { case (n, v) => n.copy(velocity = v) }
-
-  } else notesOut0
+  implicit val rnd  = new util.Random(SEED)
+  val gen       = HorizontalDance(startIdx = START, modelVelo = VELO, veloCoarse = VELO_COARSE, modelEntry = ENTRY,
+  entryCoarse = ENTRY_COARSE, entryScale = ENTRY_SCALE, noteSnippets = mm)
+  val notesOut  = gen.move(NUM)
 
   implicit val rate = TickRate.tempo(120, 1024)
-
-//  val sorted = notesIn.sortBy(_.offset)
-//  assert(notesIn == sorted)
-
-  val notesOut  = if (ENTRY) {
-    val entrySq = notesIn.sliding(2, 1).to[Vector].map { case Seq(a, b) =>
-      val fine    = ((b.offset - a.offset) * 1000 + 0.5).toInt // millis
-      val coarse  = (fine * ENTRY_COARSE + 0.5).toInt
-      if (coarse > 0) (fine - fine % coarse) else fine
-//      millis * 1000
-    }
-
-//    println(entrySq.mkString(", "))
-//    ContextDance.DEBUG = true
-    val recEntry = ContextDance.move(entrySq :+ entrySq.head, num = NUM)(entrySq(START) :: Nil)
-//    println(recEntry.mkString(", "))
-    var off = 0.0
-    (notesOut1 zip recEntry).map { case (n, e) =>
-      val res = n.copy(offset = off)
-      off += e * 0.001 * ENTRY_SCALE
-      res
-    }
-
-  } else notesOut1
-
   val events  = notesOut.flatMap(_.toMIDI)
   val track   = Track(events)
   val sqOut   = Sequence(Vector(track))
--- src/main/scala/at/iem/point/eh/sketches/HorizontalDance.scala
+++ src/main/scala/at/iem/point/eh/sketches/HorizontalDance.scala
@@ -1,58 +1,77 @@
-//package at.iem.point.eh.sketches
-//
-//import at.iem.point.illism.OffsetNote
-//import de.sciss.midi.TickRate
-//
-//object HorizontalDance {
-//  def apply(): HorizontalDance = ???
-//
-//  private final class Impl(m: Vector[Vector[OffsetNote]]) extends HorizontalDance {
-//    def move(n: Int): Vector[OffsetNote] = {
-//      val notesIn   = m.flatten //  m.flatMap(_._2)
-//      val pitchSq   = notesIn.map(_.pitch.midi)
-//
-//      val recPch    = ContextDance.move(pitchSq, num = NUM, seed = SEED)(pitchSq(START) :: Nil)
-//
-//      val notesOut0 = recPch.zipWithIndex.map { case (midi, idx) =>
-//        val off = idx * 0.25
-//        OffsetNote(off, midi.asPitch, 0.125, 80)
-//      }
-//
-//      val notesOut1 = if (VELO) {
-//        val veloSq  = notesIn.map { n => val v = n.velocity; v - (v % VELO_COARSE) }
-//        val recVelo = ContextDance.move(veloSq, num = NUM, seed = SEED)(veloSq(START) :: Nil)
-//        (notesOut0 zip recVelo).map { case (n, v) => n.copy(velocity = v) }
-//
-//      } else notesOut0
-//
-//      implicit val rate = TickRate.tempo(120, 1024)
-//
-//    //  val sorted = notesIn.sortBy(_.offset)
-//    //  assert(notesIn == sorted)
-//
-//      val notesOut  = if (ENTRY) {
-//        val entrySq = notesIn.sliding(2, 1).to[Vector].map { case Seq(a, b) =>
-//          val fine    = ((b.offset - a.offset) * 1000 + 0.5).toInt // millis
-//          val coarse  = (fine * ENTRY_COARSE + 0.5).toInt
-//          if (coarse > 0) (fine - fine % coarse) else fine
-//    //      millis * 1000
-//        }
-//
-//    //    println(entrySq.mkString(", "))
-//    //    ContextDance.DEBUG = true
-//        val recEntry = ContextDance.move(entrySq :+ entrySq.head, num = NUM, seed = SEED)(entrySq(START) :: Nil)
-//    //    println(recEntry.mkString(", "))
-//        var off = 0.0
-//        (notesOut1 zip recEntry).map { case (n, e) =>
-//          val res = n.copy(offset = off)
-//          off += e * 0.001 * ENTRY_SCALE
-//          res
-//        }
-//
-//      } else notesOut1
-//    }
-//  }
-//}
-//sealed trait HorizontalDance {
-//  def move(n: Int): Vector[OffsetNote]
-//}
+package at.iem.point.eh.sketches
+
+import at.iem.point.illism._
+import de.sciss.midi.TickRate
+
+object HorizontalDance {
+  def apply(startIdx: Int = 0, modelVelo: Boolean = true, veloCoarse: Int = 4, modelEntry: Boolean = true,
+                             entryCoarse: Double = 0.2, entryScale: Double = 1.0,
+                             noteSnippets: IIdxSeq[IIdxSeq[OffsetNote]])(implicit rnd: util.Random): HorizontalDance =
+    new Impl(startIdx = startIdx, modelVelo = modelVelo, veloCoarse = veloCoarse, modelEntry = modelEntry,
+             entryCoarse = entryCoarse, entryScale = entryScale, m = noteSnippets)
+
+  private final class Impl(startIdx: Int, modelVelo: Boolean, veloCoarse: Int, modelEntry: Boolean,
+                           entryCoarse: Double, entryScale: Double,
+                           m: IIdxSeq[IIdxSeq[OffsetNote]])(implicit rnd: util.Random)
+    extends HorizontalDance {
+
+    private val notesIn   = m.flatten // m.flatMap(_._2)
+
+    private val pitchSq   = notesIn.map(_.pitch.midi)
+    private val recPchF   = ContextDance(pitchSq)(pitchSq(startIdx) :: Nil)
+
+    private val veloSq    = notesIn.map { n => val v = n.velocity; v - (v % veloCoarse) }
+    private val recVeloF  = ContextDance(veloSq)(veloSq(startIdx) :: Nil)
+
+    private val entrySq   = notesIn.sliding(2, 1).to[Vector].map { case Seq(a, b) =>
+      val fine    = ((b.offset - a.offset) * 1000 + 0.5).toInt // millis
+      val coarse  = (fine * entryCoarse + 0.5).toInt
+      if (coarse > 0) (fine - fine % coarse) else fine
+//      millis * 1000
+    }
+    private val recEntryF = ContextDance(entrySq :+ entrySq.head)(entrySq(startIdx) :: Nil)
+
+    private var produced  = 0
+
+    def move(num: Int): Vector[OffsetNote] = {
+      val recPch = recPchF.move(num)
+
+      val notesOut0 = recPch.zipWithIndex.map { case (midi, idx) =>
+        val off = (idx + produced) * 0.25
+        OffsetNote(off, midi.asPitch, 0.125, 80)
+      }
+
+      val notesOut1 = if (modelVelo) {
+        val veloSq  = notesIn.map { n => val v = n.velocity; v - (v % veloCoarse) }
+        val recVelo = recVeloF.move(num = num)
+        (notesOut0 zip recVelo).map { case (n, v) => n.copy(velocity = v) }
+
+      } else notesOut0
+
+      implicit val rate = TickRate.tempo(120, 1024)
+
+    //  val sorted = notesIn.sortBy(_.offset)
+    //  assert(notesIn == sorted)
+
+      val notesOut  = if (modelEntry) {
+    //    println(entrySq.mkString(", "))
+    //    ContextDance.DEBUG = true
+        val recEntry = recEntryF.move(num = num)
+    //    println(recEntry.mkString(", "))
+        var off = 0.0
+        (notesOut1 zip recEntry).map { case (n, e) =>
+          val res = n.copy(offset = off)
+          off += e * 0.001 * entryScale
+          res
+        }
+
+      } else notesOut1
+
+      produced += num
+      notesOut
+    }
+  }
+}
+sealed trait HorizontalDance {
+  def move(n: Int): Vector[OffsetNote]
+}
