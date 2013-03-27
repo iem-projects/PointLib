3c80409f59a05124e662cb982b685393e7dddcce
Sciss <contact@sciss.de>
2013-03-27 10:34:25 +0100

    ...

--- src/main/scala/at/iem/point/eh/sketches/ChordUtil.scala
+++ src/main/scala/at/iem/point/eh/sketches/ChordUtil.scala
@@ -18,7 +18,7 @@ object ChordUtil {
       // includes the head note itself.
       val (fit1, rest1) = ns.span(n => math.abs(n.offset - h.offset) <= offsetTolerance && n.offset < h.stop)
       // then from those notes that fit wrt offset, filter those which satisfy the
-      // stop tolerance.                                                                                                                            s, dur =
+      // stop tolerance.
       val (fit, rest2)  = fit1.span(n => math.abs(n.stop - h.stop ) <= stopTolerance  )
       // if the minimum polyphony is found, add a new chord, and glue the two rest bins together
       val next = if (fit.size >= minPoly) {
