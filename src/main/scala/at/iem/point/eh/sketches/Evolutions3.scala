70176349d0e07a7ad57a1c851e377bdcec81d8f7
Sciss <contact@sciss.de>
2013-03-27 16:30:49 +0100

    ok, with entries better modelled. still wonder why there are so many octaves in the improv material. perhaps it all resolves when interleaving with the horizontal sequences

--- src/main/scala/at/iem/point/eh/sketches/Evolutions3.scala
+++ src/main/scala/at/iem/point/eh/sketches/Evolutions3.scala
@@ -13,7 +13,8 @@ object Evolutions3 extends App {
   val ENTRY_COARSE= 0.2     // entry offset rasterisation (relative, in percent 0...1)
   val ENTRY_SCALE = 1.5     // slow down factor if using entry modelling
 
-  val INNER       = true   // model inner structure of chords (`true`) or just frame intervals (`false`)
+  val INNER       = true    // model inner vertical structure of chords (`true`) or just frame intervals (`false`)
+  val GRAINS      = true    // model inner horizontal structure of chords (`true`) or just produce monolithic blocks (`false`)
 
   val snippet   = improvSnippets.last  // staticChords(5).head
   val sq        = loadSnippet(snippet) // .head
@@ -85,7 +86,7 @@ object Evolutions3 extends App {
   }
 
   val chordsOut1 = if (VELO) {
-    // a simple occurrence map. num voices -> velocities
+    // a simple occurrence map. num voices -> chords (velocities)
     var velos   = Map.empty[Int, IIdxSeq[Chord]] withDefaultValue Vector.empty
     chordsIn.foreach { c =>
       velos = velos + (c.size -> (velos(c.size) :+ c))
@@ -104,6 +105,28 @@ object Evolutions3 extends App {
 
   implicit val rate = TickRate.tempo(120, 1024)
 
+  val chordsOut2 = if (GRAINS) {
+    // a simple occurrence map. num voices -> chords (inner times)
+    var grains = Map.empty[Int, IIdxSeq[Chord]] withDefaultValue Vector.empty
+    chordsIn.foreach { c =>
+      grains = grains + (c.size -> (grains(c.size) :+ c))
+    }
+    chordsOut1.map { c =>
+      val cg = grains(c.size).choose
+      val ng = cg.notes.scramble
+      val oc = c.minOffset
+      val og = cg.minOffset
+      val d  = oc - og
+      Chord((c.notes zip ng).map { case (na, nb) =>
+        val d = c.minOffset
+        na.copy(offset = nb.offset + d, duration = nb.duration)
+      })
+    }
+
+  } else {
+    chordsOut1
+  }
+
   val chordsOut = if (ENTRY) {
     val entrySq = h.flatMap { case (_, cs) =>
       cs.sliding(2, 1).to[Vector].flatMap {
@@ -120,7 +143,7 @@ object Evolutions3 extends App {
     val recEntry = ContextDance.move(entrySq :+ entrySq.head, num = NUM, seed = SEED)(entrySq(START) :: Nil)
 //    println(recEntry.mkString(", "))
     var off = 0.0
-    (chordsOut1 zip recEntry).map { case (c, e) =>
+    (chordsOut2 zip recEntry).map { case (c, e) =>
       val delta = off - c.minOffset
       val res = Chord(c.notes.map(n => n.copy(offset = n.offset + delta)))
       off += (e * 0.001 + (c.maxStop - c.minOffset)) * ENTRY_SCALE
@@ -128,13 +151,15 @@ object Evolutions3 extends App {
     }
 
   } else
-    chordsOut1
+    chordsOut2
 
   val events  = chordsOut.flatMap(_.toMIDI)
   val track   = Track(events)
   val sqOut   = Sequence(Vector(track))
 
-  sqOut.writeFile(outPath / s"Snippet${snippet}_ChordSeq_${START}_${SEED}${if (INNER) "I" else ""}${if (VELO) "V" else ""}${if (ENTRY) "E" else ""}.mid")
+  sqOut.writeFile(outPath /
+    (s"Snippet${snippet}_ChordSeq_${START}_${SEED}${if (INNER) "I" else ""}" +
+     s"${if (GRAINS) "G" else ""}${if (VELO) "V" else ""}${if (ENTRY) "E" else ""}.mid"))
 
   val player  = Sequencer.open()
   player.play(sqOut)
