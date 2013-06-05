bab6b4035bce776b7b8de7b37199e026f5ecdfd7
Sciss <contact@sciss.de>
2013-06-05 17:49:16 +0200

    pas mal

--- src/main/scala/at/iem/point/sh/sketches/gui/EvaluationSettingsFrame.scala
+++ src/main/scala/at/iem/point/sh/sketches/gui/EvaluationSettingsFrame.scala
@@ -1,16 +1,39 @@
 package at.iem.point.sh.sketches
 package gui
 
-import scala.swing.{Orientation, BoxPanel, ComboBox}
+import scala.swing.{Label, Component, Orientation, BoxPanel, ComboBox}
 import genetic._
 import de.sciss.desktop.impl.WindowImpl
 import de.sciss.desktop.Window
 import scala.swing.event.SelectionChanged
+import javax.swing.SpinnerNumberModel
 
 class EvaluationSettingsFrame() {
   var eval: Evaluation = WindowedEvaluation()
 
   val pane = new BoxPanel(Orientation.Vertical)
+
+  def buildMetaGUI(meta: Meta[_]): Component = {
+    val p   = new BoxPanel(Orientation.Vertical)
+    val lb  = new Label(s"<html><body>${meta.name}<b></b>")
+    p.contents += lb
+    meta.defaults.zipWithIndex.foreach { case (arg, i) =>
+      val gg = arg match {
+        case hm: HasMeta[_] =>
+          buildMetaGUI(hm.meta)
+        case i: Int =>
+          val sm = new SpinnerNumberModel(i, Int.MinValue, Int.MaxValue, 1)
+          val _gg = new Spinner(sm)
+          _gg
+        case d: Double =>
+          val sm = new SpinnerNumberModel(d, Double.NegativeInfinity, Double.PositiveInfinity, 0.1)
+          val _gg = new Spinner(sm)
+          _gg
+      }
+      p.contents += gg
+    }
+    p
+  }
 
   val combo = new ComboBox[Meta[Evaluation]](Evaluation.all) {
     listenTo(selection)
@@ -20,13 +43,8 @@ class EvaluationSettingsFrame() {
         eval = selection.item.instance()
         val cnt = pane.contents.size
         if (cnt > 1) pane.contents.remove(1, cnt - 1)
-        eval.meta.defaults.zipWithIndex.foreach { case (arg, i) =>
-          arg match {
-            case hm: HasMeta[_] => println("has-meta")
-            case i: Int         => println("int")
-            case d: Double      => println("double")
-          }
-        }
+        pane.contents += buildMetaGUI(eval.meta)
+        // XXX frame.pack()
     }
   }
 
