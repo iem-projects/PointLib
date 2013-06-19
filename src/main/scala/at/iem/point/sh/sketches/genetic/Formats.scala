20d145615498eebdd32dd36516f17794c5f8413e
Sciss <contact@sciss.de>
2013-06-19 13:16:21 +0200

    ...

--- src/main/scala/at/iem/point/sh/sketches/genetic/Formats.scala
+++ src/main/scala/at/iem/point/sh/sketches/genetic/Formats.scala
@@ -2,16 +2,17 @@ package at.iem.point.sh.sketches.genetic
 
 import play.api.libs.json.{Format, SealedTraitFormat}
 
+// note: running into SI-7588 - when this happens, comment out all below, and slowly uncomment it back...
 object Formats {
   implicit val scaleFunction        : Format[ScaleFunction]       = SealedTraitFormat[ScaleFunction]
   implicit val matchFunction        : Format[MatchFunction]       = SealedTraitFormat[MatchFunction]
   implicit val percentage           : Format[Percentage]          = SealedTraitFormat[Percentage]
 
-  implicit val selectionSize   : Format[SelectionSize]       = SealedTraitFormat[SelectionSize]
-  implicit val selection       : Format[Selection]           = SealedTraitFormat[Selection]
+  implicit val selectionSize        : Format[SelectionSize]       = SealedTraitFormat[SelectionSize]
+  implicit val selection            : Format[Selection]           = SealedTraitFormat[Selection]
 
   implicit lazy val breedingFunction: Format[BreedingFunction]    = SealedTraitFormat[BreedingFunction]
-  implicit val breeding        : Format[Breeding]            = SealedTraitFormat[Breeding]
+  implicit val breeding             : Format[Breeding]            = SealedTraitFormat[Breeding]
 
   implicit val chromosomeFunction   : Format[ChromosomeFunction]  = SealedTraitFormat[ChromosomeFunction]
   implicit val aggregateFunction    : Format[AggregateFunction]   = SealedTraitFormat[AggregateFunction]
