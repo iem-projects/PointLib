2ca3311937ca5a93cdb0c9ba7928551909fcaaeb
Sciss <contact@sciss.de>
2013-07-02 17:09:26 +0200

    step

--- src/main/scala/at/iem/point/er/sketches/MultiResOnsets.scala
+++ src/main/scala/at/iem/point/er/sketches/MultiResOnsets.scala
@@ -8,27 +8,27 @@ object MultiResOnsets {
   def apply(product: OnsetsAnalysisWindow.Product, config: Config = Config()): MultiResOnsets = {
     import config._
 
-    @tailrec def loop(res: Vec[(Long, Int)], p: OnsetsAnalysisWindow.Product, lvl: Int): Vec[(Long, Int)] = p match {
+    @tailrec def loop(res: Vec[Entry], p: OnsetsAnalysisWindow.Product, lvl: Int): Vec[Entry] = p match {
       case (_cfg, Some(_o)) +: tail =>
         val cfg: OnsetsAnalysis.Config = _cfg   // sucky IDEA
         val o: Vec[Long] = _o                   // sucky IDEA
         val maxFrame = cfg.fftSize / cfg.fftOverlap * maxDrift
-        val (lost, next) = ((res, Vec.empty[(Long, Int)]) /: o) {
+        val (lost, next) = ((res, Vec.empty[Entry]) /: o) {
           case ((rem, add), pos) =>
             val cand = rem.zipWithIndex.collect {
-              case ((pos1, _), idx) if math.abs(pos1 - pos) <= maxFrame => math.abs(pos1 - pos) -> idx
+              case (entry, idx) if math.abs(entry.pos - pos) <= maxFrame => math.abs(entry.pos - pos) -> idx
             }
             if (cand.isEmpty) {
-              (rem, add :+ (pos, 1))
+              (rem, add :+ Entry(pos, lvl, lvl + 1))
             } else {
               val idx     = cand.minBy(_._1)._2
-              val (_, up) = rem(idx)
+              val up      = rem(idx)
               val rem1    = rem.patch(idx, Vec.empty, 1)
-              (rem1, add :+ (pos, up + 1))
+              (rem1, add :+ up.bump(pos))
             }
         }
-        if (lost.nonEmpty) println(s"Warning: lost ${lost.mkString(", ")} in level ${lvl + 1}")
-        loop(next, tail, lvl + 1)
+        // if (lost.nonEmpty) println(s"Warning: lost ${lost.mkString(", ")} in level ${lvl + 1}")
+        loop((lost ++ next).sortBy(_.pos), tail, lvl + 1)
 
       case _ :+ _ => sys.error(s"No onsets in level ${lvl + 1}")
       case _ => res
@@ -36,5 +36,10 @@ object MultiResOnsets {
 
     MultiResOnsets(product.size, loop(Vec.empty, product, 0))
   }
+
+  case class Entry(pos: Long, from: Int, to: Int) {
+    def levels: Int = to - from
+    def bump(pos: Long): Entry = copy(to = to + 1)
+  }
 }
-case class MultiResOnsets(levels: Int, onsets: Vec[(Long, Int)])
+case class MultiResOnsets(levels: Int, onsets: Vec[MultiResOnsets.Entry])
