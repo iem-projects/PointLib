dbb66097f5c26a81e364467d29d4758c3822b1cb
Sciss <contact@sciss.de>
2013-07-29 18:41:54 +0200

    all interval xkorr plot

--- src/main/scala/at/iem/point/ot/sketches/Analysis.scala
+++ src/main/scala/at/iem/point/ot/sketches/Analysis.scala
@@ -1,9 +1,22 @@
 package at.iem.point.ot.sketches
 
 import scala.swing.{Frame, SimpleSwingApplication}
+import at.iem.point.illism.chart.HarmonicFields
+import scala.swing.event.WindowClosing
+import de.sciss.pdflitz
 
 object Analysis extends SimpleSwingApplication {
   lazy val top = new Frame {
+    val chart = HarmonicFields.contingencyChart(chordSeq, allIntervals = true, title = "All intervals")
+    contents = chart
+    pack()
+    centerOnScreen()
+    open()
 
+    reactions += {
+      case WindowClosing(_) => quit()
+    }
+
+    new pdflitz.SaveAction(chart :: Nil).setupMenu(this)
   }
 }
