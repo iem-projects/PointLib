715ab6b88db853862fa5b144888f273167ff37aa
Sciss <contact@sciss.de>
2013-09-24 16:36:54 +0200

    muta don't touch good chords

--- src/main/scala/at/iem/point/ot/sketches/GeneticSystem.scala
+++ src/main/scala/at/iem/point/ot/sketches/GeneticSystem.scala
@@ -7,11 +7,10 @@ import at.iem.point.illism._
 import scala.util.{Success, Failure, Try, Random}
 import de.sciss.muta.{SelectionNumber, SelectionPercent, SelectionSize}
 import scala.swing.{Component, Label}
-import scala.annotation.tailrec
 import play.api.libs.json.{JsSuccess, JsError, JsString, JsObject, JsResult, JsArray, JsValue, Format, SealedTraitFormat}
 import collection.breakOut
 import de.sciss.jacop
-import JaCoP.search.{IndomainRandom, SmallestDomain, IndomainMin, SimpleSelect}
+import JaCoP.search.{SmallestDomain, SimpleSelect}
 
 case class Voice(maxUp: Int = 2, maxDown: Int = 2, lowest: Int = 36, highest: Int = 96)
 
@@ -98,45 +97,151 @@ case class BreedingImpl(elitism       : SelectionSize     = SelectionNumber(20),
                         mutation       : BreedingFunctionImpl  = Mutation())
   extends muta.impl.BreedingImpl[GeneticSystem.Chromosome, GlobalImpl]
 
-object Crossover extends muta.impl.CrossoverVecImpl[(Chord, ChordEval), GlobalImpl] with BreedingFunctionImpl
+object Crossover extends muta.impl.CrossoverVecImpl[GeneticSystem.Gene, GlobalImpl] with BreedingFunctionImpl
 
 case class Mutation(chords: SelectionSize = SelectionPercent(20),
                     voices: SelectionSize = SelectionNumber(3), interval: Int = 7)
-  extends muta.impl.MutationVecImpl [(Chord, ChordEval), GlobalImpl] with BreedingFunctionImpl {
+  extends BreedingFunctionImpl {
 
-  override protected val numGenesSize = chords
+  def numGenesSize = chords
 
-  def mutate(gene0: (Chord, ChordEval))(implicit r: util.Random): (Chord, ChordEval) = {
-    // println("Muta!")
-    val gene = gene0._1
+  import GeneticSystem.{Gene, Chromosome, Genome, Global}
 
-    val num       = r.nextInt(math.max(0, voices(gene.size) - 1) + 1)
-    val pitches   = gene.pitches.scramble
-    val (a, b)    = pitches.splitAt(num)
-    val bMid      = b.map(_.midi)
+  def apply(gen: Genome, num: Int, glob: Global, r: util.Random): Genome = Vec.fill(num) {
+    val i   = r.nextInt(gen.size)
+    val gi  = gen(i)
+    val mut = numGenes(gi)(r)
 
-    @tailrec def mkMut(): Vec[Pitch] = {
-      val mut       = a.map { p =>
-        val old     = p.midi
-        val poss    = (old - interval to old + interval) diff (bMid :+ old)
-
-        val nu = if (poss.isEmpty) old else {
-          val i     = r.nextInt(poss.size)
-          poss(i)
+    (gi /: (1 to mut)) { (gj, _) =>
+      var tries = 1000
+      var res   = gj
+      while (tries > 0) {
+        val j     = r.nextInt(gj.size)
+        val in    = gj(j)
+        if (in._2 == ChordGood) {
+          tries -= 1
+        } else {
+          val pred  = if (j > 0          ) Some(gj(j - 1)) else None
+          val succ  = if (j < gj.size - 1) Some(gj(j + 1)) else None
+          val m     = mutate(glob, in, pred, succ)(r)
+          res       = gj.updated(j, m)
+          tries     = 0
         }
-        nu.asPitch
       }
-      if (mut == mut.distinct) mut else mkMut() // cheesy way to repeat if there are duplicates
+      res
     }
-
-    val newP    = mkMut() ++ b
-    val notes   = (gene.notes zip newP).map {
-      case (n, p) => n.copy(pitch = p)
-    }
-
-    val gene1 = gene.copy(notes = notes.sortBy(_.pitch))
-    (gene1, if (gene1 == gene) gene0._2 else ChordNeutral)
   }
+
+  /** The number of genes to mutate, given a particular chromosome. Defaults to
+    * applying `numGenesSize` with the chromosome size.
+    */
+  protected def numGenes(chromosome: Chromosome)(implicit random: util.Random): Int =
+    random.nextInt(numGenesSize(chromosome.size))
+
+  def mutate(glob: Global, gene: Gene, pred: Option[Gene], succ: Option[Gene])(implicit r: util.Random): Gene = {
+    val (chord, _) = gene
+    val csz   = chord.size
+    require(csz == glob.voices.size)
+
+    val num   = voices(csz)
+    val sel   = Vec.tabulate(csz)(_ < num).scramble
+
+    import jacop._
+    implicit val model = new Model
+    import Implicits._
+
+    val pitchesSel = chord.pitches.reverse.zip(sel)
+    val vars = pitchesSel.zip(glob.voices).map { case ((p, psel), vc) =>
+      val midi = p.midi
+      if (psel) {
+        val lo = math.max(vc.lowest , midi - interval)
+        val hi = math.min(vc.highest, midi + interval)
+        assert(lo <= hi)
+        new IntVar(lo, hi)
+      } else new IntVar(midi, midi)
+    }
+    vars.foreachPair{ case (hi, lo) =>
+      hi #> lo
+    }
+
+    pred.foreach { case (predC, _) =>
+      vars.zip(predC.pitches.reverse).zip(glob.voices).foreach { case ((iv, predP), vc) =>
+        val pp = predP.midi
+        iv #<= (pp + vc.maxUp  )
+        iv #>= (pp - vc.maxDown)
+      }
+    }
+    // something with the following is wrong probably:
+    succ.foreach { case (succC, _) =>
+      vars.zip(succC.pitches.reverse).zip(glob.voices).foreach { case ((iv, succP), vc) =>
+        val sp = succP.midi
+        iv #<= (sp - vc.maxUp  )
+        iv #>= (sp + vc.maxDown)
+      }
+    }
+
+    require(model.consistency(), s"Constraints model is not consistent")
+
+    def mkChord(): Chord = {
+      val pitches = vars.map(v => v.value().asPitch).reverse
+      val notes   = pitches.map(pitch => OffsetNote(offset = 0, pitch = pitch, duration = 1, velocity = 80))
+      Chord(notes)
+    }
+
+    val select        = new SimpleSelect[IntVar](vars.toArray,
+      new SmallestDomain[IntVar](),
+      new IndomainRandom2(r) // new IndomainMin[IntVar]()
+    )
+    val solutionsB    = Vec.newBuilder[Chord]
+    limitOnSolutions  = 256
+    timeOutValue      = 10    // seconds
+    val result        = satisfyAll[IntVar](select, () => solutionsB += mkChord())
+    val solutions     = solutionsB.result()
+
+    if (!result) {
+      val thisInfo = pitchesSel.reverse.map { case (p, s) => s"$p${if (s) "!" else ""}" } .mkString(", ")
+      val predInfo = pred.fold("") { case (c, _) =>
+        c.pitches.mkString("; pred = ", ", ", "")
+      }
+      val succInfo = succ.fold("") { case (c, _) =>
+        c.pitches.mkString("; pred = ", ", ", "")
+      }
+      println(s"Warning: could not mutate $thisInfo$predInfo$succInfo")
+      return gene
+    }
+
+    val chordOut = solutions.choose(r)
+    println(s"Mutated: ${chordOut.pitches.mkString(", ")}")
+    chordOut -> ChordNeutral
+  }
+
+  //  def mutateOLD(gene0: Gene, pred: Option[Gene], succ: Option[Gene])(implicit r: util.Random): Gene = {
+  //    val pitches   = gene.pitches.scramble
+  //    val (a, b)    = pitches.splitAt(num)
+  //    val bMid      = b.map(_.midi)
+  //
+  //    @tailrec def mkMut(): Vec[Pitch] = {
+  //      val mut       = a.map { p =>
+  //        val old     = p.midi
+  //        val poss    = (old - interval to old + interval) diff (bMid :+ old)
+  //
+  //        val nu = if (poss.isEmpty) old else {
+  //          val i     = r.nextInt(poss.size)
+  //          poss(i)
+  //        }
+  //        nu.asPitch
+  //      }
+  //      if (mut == mut.distinct) mut else mkMut() // cheesy way to repeat if there are duplicates
+  //    }
+  //
+  //    val newP    = mkMut() ++ b
+  //    val notes   = (gene.notes zip newP).map {
+  //      case (n, p) => n.copy(pitch = p)
+  //    }
+  //
+  //    val gene1 = gene.copy(notes = notes.sortBy(_.pitch))
+  //    (gene1, if (gene1 == gene) gene0._2 else ChordNeutral)
+  //  }
 }
 
 /** Constraint on the vertical (harmonic) structure. */
@@ -211,7 +316,8 @@ object GeneticSystem extends muta.System {
   )
 
   type Global     = GlobalImpl
-  type Chromosome = Vec[(Chord, ChordEval)]
+  type Gene       = (Chord, ChordEval)
+  type Chromosome = Vec[Gene]
   type Generation = GenerationImpl
   type Evaluation = EvaluationImpl
   type Selection  = SelectionImpl
