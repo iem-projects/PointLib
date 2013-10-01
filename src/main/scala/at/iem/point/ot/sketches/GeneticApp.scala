b9902ad76d5420173d5d58b982ae53f47538bc27
Sciss <contact@sciss.de>
2013-10-01 14:40:24 +0200

    pdf screenshot (not working yet due to music font mapping missing)

--- src/main/scala/at/iem/point/ot/sketches/GeneticApp.scala
+++ src/main/scala/at/iem/point/ot/sketches/GeneticApp.scala
@@ -1,7 +1,26 @@
 package at.iem.point.ot.sketches
 
 import de.sciss.muta
+import de.sciss.desktop.{FileDialog, Menu}
 
 object GeneticApp extends muta.gui.GeneticApp(GeneticSystem) {
   override def rowHeight = 176 // 128 // 64
+
+
+  protected override def init(): Unit = {
+    import Menu._
+    val root = menuFactory
+    root.add(Group("extra", "Extra")
+      .add(Item("screenshot")("Save PDF Screenshot...")(saveScreenshot()))
+    )
+  }
+
+  def saveScreenshot(): Unit =
+    windowHandler.windows.toList.headOption.foreach { w =>
+      val dlg = FileDialog.save()
+      dlg.show(Some(w)).foreach { file =>
+        val view = w.component.contents.head
+        de.sciss.pdflitz.Generate(file, view)
+      }
+    }
 }
