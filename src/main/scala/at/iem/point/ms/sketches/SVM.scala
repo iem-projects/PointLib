6cbfadcded7bfc1dd717527c07debc7b4c4cf575
Sciss <contact@sciss.de>
2013-11-12 12:58:06 +0100

    no luck with tpe and kernel variations

--- src/main/scala/at/iem/point/ms/sketches/SVM.scala
+++ src/main/scala/at/iem/point/ms/sketches/SVM.scala
@@ -2,11 +2,45 @@ package at.iem.point.ms.sketches
 
 import de.sciss.file._
 import java.io.PrintStream
+import sys.process._
 
 object SVM extends App {
   def svmDir      = userHome / "Documents" / "devel" / "libsvm"
   def svmTrain    = svmDir / "svm-train"
   def svmPredict  = svmDir / "svm-predict"
+  def svmScale    = svmDir / "svm-scale"
+  def scale       = false   // doesn't improve, basically we're already scaled
+
+  object SvmType {
+    case object C_SVC    extends SvmType { def optionID = 0 }
+    case object Nu_SVC   extends SvmType { def optionID = 1 }
+    case object OneClass extends SvmType { def optionID = 2 }
+    case object Eps_SVR  extends SvmType { def optionID = 3 }
+    case object Nu_SVR   extends SvmType { def optionID = 4 }
+  }
+  sealed trait SvmType {
+    def toOptions: List[String] = "-s" :: optionID.toString :: Nil
+    def optionID: Int
+  }
+
+  object Kernel {
+    case object Linear  extends Kernel { def optionID = 0 }
+    case object Poly    extends Kernel { def optionID = 1 }
+    case object Radial  extends Kernel { def optionID = 2 }
+    case object Sigmoid extends Kernel { def optionID = 3 }
+    case object Pre     extends Kernel { def optionID = 4 }
+  }
+  sealed trait Kernel {
+    def toOptions: List[String] = "-t" :: optionID.toString :: Nil
+    def optionID: Int
+  }
+
+  object TrainOptions {
+    var tpe   : SvmType = SvmType.C_SVC   // default: C_SVC
+    var kernel: Kernel  = Kernel.Radial   // default: Radial
+
+    def toOptions: List[String] = tpe.toOptions ++ kernel.toOptions
+  }
 
   def kreuzVec(map: Map[Int, Map[Int, Int]]): Vec[Vec[Int]] =
     Vec.tabulate(7)(x => Vec.tabulate(x + 1)(y => map(x)(y)))
@@ -18,7 +52,7 @@ object SVM extends App {
   }
 
   def svmString(boring: Boolean, vec: Vec[Double]): String = {
-    val categ = if (boring) "0" else "1"
+    val categ = if (boring) 0 else 1
     vec.zipWithIndex.map { case (num, fi) => s"${fi + 1}:$num" } .mkString(s"$categ ", " ", "")
   }
 
@@ -49,22 +83,30 @@ object SVM extends App {
   val fPredict  = tempFile("predict")
   
   def svmWrite(file: File, lines: Vec[String]): Unit = {
-    val out = new PrintStream(file, "UTF-8")
+    val f1  = if (scale) File.createTemp() else file
+    val out = new PrintStream(f1, "UTF-8")
     lines.foreach(out.println)
     out.close()
+    if (scale) {
+      val scaled  = Seq(svmScale.path, file.path).!!
+      val out1    = new PrintStream(file, "UTF-8")
+      out1.print(scaled)
+      out1.close()
+    }
   }
 
-  import sys.process._
+  def dataPath(f: File): String = f.path
+    // (if (scale) f.parent / s"${f.base}.scale" else f).path
 
   svmWrite(fTrain, allTrain)
   svmWrite(fTest , allTest )
 
   println("\n\nRunning SVM Analysis\n")
   
-  val resTrain = Seq(svmTrain.path, fTrain.path, fModel.path).!
+  val resTrain = (Seq(svmTrain.path) ++ TrainOptions.toOptions ++ Seq(dataPath(fTrain), fModel.path)).!
   require(resTrain == 0, s"svm-train failed with code $resTrain")
 
-  val resPredict = Seq(svmPredict.path, fTest.path, fModel.path, fPredict.path).!
+  val resPredict = Seq(svmPredict.path, dataPath(fTest), fModel.path, fPredict.path).!
   require(resPredict == 0, s"svm-predict failed with code $resTrain")
 
   // File.revealInFinder(svmIn)
