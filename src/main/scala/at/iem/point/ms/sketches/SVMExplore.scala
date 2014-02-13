c8006db22a0f886f79b1f89975a953aee940a3b4
Sciss <contact@sciss.de>
2014-02-13 15:04:27 +0100

    yo bruta fuerza

--- src/main/scala/at/iem/point/ms/sketches/SVMExplore.scala
+++ src/main/scala/at/iem/point/ms/sketches/SVMExplore.scala
@@ -4,6 +4,7 @@ import scala.swing._
 import de.sciss.numbers.Implicits._
 import java.awt.EventQueue
 import scala.swing.event.ButtonClicked
+import scala.concurrent.{blocking, ExecutionContext, Future}
 
 object SVMExplore extends SimpleSwingApplication {
   val problems: Vec[SVM.Problem] = SVM.normalize(SVM.allProblems)
@@ -30,12 +31,19 @@ object SVMExplore extends SimpleSwingApplication {
       applet.addPoint(x.value, y.value, p.label)
     }
     applet.repaint()
-    // applet.runAnalysis()
+    val (_, rel) = charlie(Vec(xi0, yi0))
+    setPercent(rel)
+  }
+
+  def setPercent(p: Double): Unit =
+    ggPercent.text = (p * 100).toInt.toString
+
+  def charlie(indices: Vec[Int]): (Int, Double) = {
     val param = applet.parameters()
-    val prob  = applet.mkProblem(problems)(_.label)(p => Vec(p.features(xi0).value, p.features(yi0).value))
+    val prob  = applet.mkProblem(problems)(_.label)(p => indices.map(p.features(_).value))
     val model = applet.train(prob, param)
-    val (_, _, rel) = applet.verify(model, prob)
-    ggPercent.text = (rel * 100).toInt.toString
+    val (_, abs, rel) = applet.verify(model, prob)
+    (abs, rel)
   }
 
   lazy val top: Frame = {
@@ -43,7 +51,7 @@ object SVMExplore extends SimpleSwingApplication {
     // val height    = width + 50
 
     def mkFeatureSel(): (ButtonGroup, Vec[RadioButton]) = {
-      val radios = Vector.fill(numFeatures)(new RadioButton {
+      val radios = Vec.fill(numFeatures)(new RadioButton {
         listenTo(this)
         reactions += {
           case ButtonClicked(_) => performUpdate()
@@ -56,11 +64,16 @@ object SVMExplore extends SimpleSwingApplication {
     lazy val (gx, gxr) = mkFeatureSel()
     lazy val (gy, gyr) = mkFeatureSel()
 
-    lazy val pFeat: GridPanel = new GridPanel(4, numFeatures) {
+    lazy val ggPermut: Vec[CheckBox] = Vec.fill(numFeatures)(new CheckBox)
+
+    lazy val ggPermutProg: ProgressBar = new ProgressBar
+
+    lazy val pFeat: GridPanel = new GridPanel(5, numFeatures) {
       contents ++= featureNames.map(new Label(_))
       contents ++= lb
       contents ++= gx.buttons
       contents ++= gy.buttons
+      contents ++= ggPermut
     }
 
     def performUpdate(): Unit = {
@@ -75,9 +88,62 @@ object SVMExplore extends SimpleSwingApplication {
       }
     }
 
+    def selectCombi(indices: Vec[Int]): Unit =
+      ggPermut.zipWithIndex.foreach { case (gg, i) => gg.selected = indices.contains(i) }
+
+    lazy val ggRunPermut: Button = Button("Permut") {
+      import ExecutionContext.Implicits.global
+      libsvm.svm.svm_set_print_string_function(new libsvm.svm_print_interface {
+        def print(s: String) = () // shut up
+      })
+      val res = Future {
+        var best      = 0.0
+        var bestAbs   = 0
+        var bestCombi = Vec.empty[Int]
+        for (num <- 2 to numFeatures) {
+          Swing.onEDT {
+            ggPermutProg.value = num * 100 / numFeatures
+          }
+          (0 until numFeatures).combinations(num).foreach { indices =>
+            Swing.onEDT(selectCombi(indices))
+            val (abs, rel) = blocking(charlie(indices))
+            if (rel > best) {
+              best      = rel
+              bestAbs   = abs
+              bestCombi = indices
+              Swing.onEDT {
+                setPercent(rel)
+                if (rel >= 0.9) printCombi(bestAbs, best, bestCombi)
+              }
+            }
+          }
+        }
+        (bestAbs, best, bestCombi)
+      }
+
+      res.foreach { case (bestAbs, best, bestCombi) =>
+        Swing.onEDT {
+          selectCombi(bestCombi)
+          setPercent(best)
+          printCombi(bestAbs, best, bestCombi)
+        }
+      }
+
+      res.onComplete(_ => libsvm.svm.svm_set_print_string_function(null))
+    }
+
+    def printCombi(abs: Int, rel: Double, indices: Vec[Int]): Unit = {
+      println(indices)
+      println(s"$abs out of ${problems.size} (${(rel * 100).toFloat}%)")
+    }
+
     val pFeat2: BorderPanel = new BorderPanel {
-      add(pFeat    , BorderPanel.Position.Center)
-      add(ggPercent, BorderPanel.Position.East  )
+      add(pFeat      , BorderPanel.Position.Center)
+      add(ggPercent  , BorderPanel.Position.West  )
+      add(new BorderPanel {
+        add(ggRunPermut , BorderPanel.Position.Center)
+        add(ggPermutProg, BorderPanel.Position.South )
+      }, BorderPanel.Position.East)
       // add(ggUpdate, BorderPanel.Position.East  )
     }
 
