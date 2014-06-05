2fb2bc88d9f7676c02528741ee6b21ab757d02b5
Hanns Holger Rutz <contact@sciss.de>
2014-06-05 14:43:14 +0200

    context tree example dot output basically ok; we will now try to superimpose the different depths within the same file

--- src/test/scala/at/iem/point/eh/sketches/ContextVis.scala
+++ src/test/scala/at/iem/point/eh/sketches/ContextVis.scala
@@ -7,8 +7,10 @@ import de.sciss.file._
 import java.io.FileOutputStream
 
 object ContextVis extends App {
+  val inp = improvSnippets.take(1)
+
   // use only one file to make the tree smaller
-  val allIvals = (improvSnippets.take(1)).flatMap { i =>
+  val allIvals = inp.flatMap { i =>
     val s = loadSnippet(i)
     val ivals = s.notes.map(_.pitch).pairMap(_ to _).map(_.semitones % 12)
     ivals
@@ -24,33 +26,34 @@ object ContextVis extends App {
   //  fos.write(dot.getBytes("UTF-8"))
   //  fos.close()
 
-  def explore(elem: Int): String = {
+  def explore(init: Vector[Int]): String = {
     val sb = new StringBuffer()
     sb.append("digraph suffixes {\n")
+    sb.append(s"""  node [margin=0.05 fontcolor=black fontsize=16 width=0.125 shape=circle style=filled fillcolor=lightgray];\n""")
 
     def iter(body: Vector[Int]): Unit = {
       val snake   = tree.snake(body)
       val succ    = snake.successors.toVector.histogram
       val isLeaf  = succ.size <= 1
       val id      = body.mkString("_")
-      sb.append(s"""n$id [label="${body.last}"];\n""")
+      sb.append(s"""  n$id [label="${body.last}"];\n""")
       succ.foreach { case (suf, count) =>
         val body1 = body :+ suf
         val id1   = body1.mkString("_")
-        sb.append( s"""n$id -> n$id1;\n""")
+        sb.append( s"""  n$id -> n$id1;\n""")
       }
       succ.foreach { case (suf, count) =>
         val body1 = body :+ suf
         val id1   = body1.mkString("_")
         if (isLeaf) {
-          sb.append(s"""n$id1 [label="$suf"];\n""")
-          sb.append(s"""n$id1 [shape=point];\n""")
+          sb.append(s"""  n$id1 [label="$suf"];\n""")
+          sb.append(s"""  n$id1 [shape=point];\n""")
         } else {
           iter(body1)
         }
       }
     }
-    iter(Vector(elem))
+    iter(init)
 
     sb.append("}\n")
     sb.toString
@@ -58,9 +61,16 @@ object ContextVis extends App {
 
   val dir = basePath / "new_plots"
   require(dir.isDirectory)
-  val f   = dir / "improv_context.dot"
-  val dot = explore(3)
-  val fos = new FileOutputStream(f)
-  fos.write(dot.getBytes("UTF-8"))
-  fos.close()
+
+  def mkFile(init: Vector[Int]): Unit = {
+    val f = dir / s"improv${inp.mkString("_")}_context${init.mkString("_")}.dot"
+    val dot = explore(init)
+    val fos = new FileOutputStream(f)
+    fos.write(dot.getBytes("UTF-8"))
+    fos.close()
+  }
+
+  mkFile(Vector(3))
+  mkFile(Vector(5, 5))
+  mkFile(Vector(5, 8))
 }
